5f4064c5eb0bc0589c9535f5fcdfbb5a87724f80
Samarjeet <samarjeetpandey1402@gmail.com>
2024-07-30 19:20:39 +0530

    data fetch done2

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -26,7 +26,7 @@ function FetchData() {
   if (error) return <p>Error: {error}</p>;
   return (
     <div>
-      <h1>Data Fetched from Api</h1>
+      <h1>Data Fetched from API</h1>
       {/* {loading ? <p>Loading...</p> : <p>An error occurred: {error}</p>} */}
       <pre>{data && JSON.stringify(data)}</pre>
     </div>
